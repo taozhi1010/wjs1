28b98ea424ea1a1a6123a16fefb7c959d49638ed
taozhi <805446529@qq.com>
2018-09-10 16:58:16 +0800

    第一次上传

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+     var mySwiper = new Swiper ('.swiper-container',{
+         autoplay:{
+                 delay: 2500,
+                 disableOnInteraction: false,
+             },
+             loop:true,
+             direction : 'horizontal',
+          pagination: '.swiper-pagination',
+       
+        pagination: {
+         el: '.swiper-pagination',
+         clickable:true
+         },
+
+         // Navigation arrows
+     navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+     },
+
+ 
+ })
+ mySwiper.params.pagination.clickable = true ;
+//此外还需要重新初始化pagination
+})
+
